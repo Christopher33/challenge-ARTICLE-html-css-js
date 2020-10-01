1377f007d8d779176a5f111fea7a5491af8e128e
Christopher33 <christopher.couty@lapiscine.pro>
2020-10-01 16:59:23 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function shareOpen() {
+    let element = document.getElementById("toto");
+    let element2 = document.getElementById("tata");
+    let element3 = document.getElementById("color_path");
+    let element4 = document.getElementById("toto");
+
+    element.classList.toggle("share");
+    element4.classList.toggle("share_mobile");
+    
+    element2.classList.toggle("rond_share_open");
+    element3.classList.toggle("color_path");
+}
